0919ef40ddc0055afa19b749818a930e975ee8cf
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-16 16:25:43 -0300

    feat: add error middleware to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import Factory from './factory';
+import { errorMiddleware } from './middlewares';
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/task', Factory.getTaskRouter());
+
+app.use(errorMiddleware);
 
 export default app;
