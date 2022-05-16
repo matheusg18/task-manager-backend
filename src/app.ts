16b3b1f22a43bd75f3297ad08365cc2224c82cf4
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-16 01:03:08 -0300

    feat: add task router to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import Factory from './factory';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/task', Factory.getTaskRouter());
 
 export default app;
