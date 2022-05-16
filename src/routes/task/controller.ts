96038fc95942cdc9044c86e5088dff7c18490ab2
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-16 12:20:17 -0300

    feat: add create to task controller

--- src/routes/task/controller.ts
+++ src/routes/task/controller.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from 'express';
+import { ITaskCreateRequest } from '../../interfaces/routes';
 import TaskService from './service';
 
 export default class TaskController {
@@ -12,5 +13,12 @@ export default class TaskController {
     const allTasks = await this.taskService.getAll();
 
     res.status(200).json(allTasks);
+  };
+
+  public create: RequestHandler = async (req, res) => {
+    const { content } = req.body as ITaskCreateRequest;
+    const newTask = await this.taskService.create(content);
+
+    res.status(201).json(newTask);
   };
 }
