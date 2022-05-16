3394c727037f76942af5ee7c71d215d191bdda4c
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-16 01:02:03 -0300

    feat: add GET /task

--- src/routes/task/router.ts
+++ src/routes/task/router.ts
@@ -1,9 +1,18 @@
+import { Router } from 'express';
 import TaskController from './controller';
 
 export default class TaskRouter {
+  public router = Router();
+
   private readonly taskController: TaskController;
 
   constructor(taskController: TaskController) {
     this.taskController = taskController;
+
+    this.init();
+  }
+
+  private init(): void {
+    this.router.get('/', this.taskController.getAll);
   }
 }
