3480a259af2424c4efb7d3be94bbbd1c79b39692
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-17 20:12:58 -0300

    feat: add validate params id middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { default as errorMiddleware } from './error';
 export { default as validateTaskCreate } from './validateTaskCreate';
+export { default as validateParamsId } from './validateParamsId';
