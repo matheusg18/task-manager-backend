e08615b5f98bfd6b95d4ff2131634145da2d5d5d
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-17 18:44:35 -0300

    feat: add update to task service

--- src/routes/task/service.ts
+++ src/routes/task/service.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { ITask } from '../../interfaces';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { NotFound } from '../../http-errors';
+import { ITask, ITaskUpdateRequest } from '../../interfaces';
 
 export default class TaskService {
   private readonly prisma: PrismaClient;
@@ -14,5 +16,23 @@ export default class TaskService {
 
   public async create(content: string): Promise<ITask> {
     return this.prisma.task.create({ data: { content } });
+  }
+
+  public async update(id: string, payload: ITaskUpdateRequest): Promise<ITask> {
+    const { content, status } = payload;
+
+    try {
+      const updatedTask = await this.prisma.task.update({
+        data: { content, status },
+        where: { id },
+      });
+
+      return updatedTask;
+    } catch (error) {
+      // P2025 is the error code for not found record
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
+        throw new NotFound('task not found');
+      } else throw error;
+    }
   }
 }
