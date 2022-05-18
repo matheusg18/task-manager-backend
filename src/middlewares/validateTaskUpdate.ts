b98732883c8ffaf1201209445a6ab15c29334b76
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-18 12:41:45 -0300

    feat: add validate task update middleware

---
+++ src/middlewares/validateTaskUpdate.ts
@@ -0,0 +1,27 @@
+import { RequestHandler } from 'express';
+import joi from 'joi';
+import { TASK_CONTENT_MAX_LENGTH } from '../constants';
+import { BadRequest } from '../http-errors';
+import { ITaskUpdateRequest } from '../interfaces';
+
+const schema = joi.object({
+  content: joi.string().max(TASK_CONTENT_MAX_LENGTH).messages({
+    'string.base': '"content" must be a string',
+    'string.empty': '"content" must have at least 1 character',
+    'string.max': `"content" must have at maximum ${TASK_CONTENT_MAX_LENGTH} characters`,
+  }),
+  status: joi.valid('PENDING', 'IN_PROGRESS', 'DONE').messages({
+    'any.only': '"status" can only be "PENDING", "IN_PROGRESS", "DONE"',
+  }),
+});
+
+const validateTaskUpdate: RequestHandler = (req, _res, next) => {
+  const { content, status } = req.body as ITaskUpdateRequest;
+  const { error } = schema.validate({ content, status });
+
+  if (error) return next(new BadRequest(error.message));
+
+  return next();
+};
+
+export default validateTaskUpdate;
