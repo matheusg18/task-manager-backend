9516435a2f86abae8502a37c1acaed4dba87f49b
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-18 14:57:27 -0300

    feat: add not found middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,5 @@
 export { default as errorMiddleware } from './error';
+export { default as notFound } from './notFound';
 export { default as validateTaskCreate } from './validateTaskCreate';
 export { default as validateParamsId } from './validateParamsId';
 export { default as validateTaskUpdate } from './validateTaskUpdate';
